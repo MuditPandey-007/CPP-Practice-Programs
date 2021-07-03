808acc489fb0033f2e8d181628a360e20e90a874
MuditPandey-007 <muditindian1998@gmail.com>
2021-07-02 17:35:07 -0700

    Init commit

---
+++ Practice/test1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int a;
+    cout<<"Enter a number"<<endl;
+    cin>>a;
+
+    int s=a*a;
+    cout<<"The square of "<<a<<" is "<<s;
+
+    return 0;
+}
